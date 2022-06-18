21bc29b46e1154832373f5bdcf3661b5c37a9fdc
rhaegar7996 <Mohitagarwal07091996@gmail.com>
2022-06-18 16:43:42 +0530

    Final

---
+++ Men's_Shoes.js
@@ -0,0 +1,453 @@
+ms_prod_data = [
+    {
+        product_id:"ms1",
+        price:140,
+        category:"Boots",
+        brand:"Lacoste",
+        img:"https://cdna.lystit.com/200/250/tr/photos/scottsmenswear/4415d503/lacoste-black-Urban-Breaker-Boots.jpeg"
+    },
+    {
+        product_id:"ms2",
+        price:360,
+        category:"Boots",
+        brand:"Diesel",
+        img:"https://cdna.lystit.com/200/250/tr/photos/vitkac/2536b411/diesel-BLACK-d-konba-Leather-Ankle-Boots-Black.jpeg"
+    },
+    {
+        product_id:"ms3",
+        price:122,
+        category:"Boots",
+        brand:"Lacoste",
+        img:"https://cdna.lystit.com/200/250/tr/photos/scottsmenswear/dd4072e8/lacoste-brown-Esparre-Chukka-Boots.jpeg"
+    },
+    {
+        product_id:"ms4",
+        price:291,
+        category:"Boots",
+        brand:"Diesel",
+        img:"https://cdna.lystit.com/200/250/tr/photos/vitkac/a63a82d3/diesel-BLACK-d-alabhama-Leather-Chelsea-Boots.jpeg"
+    },
+    {
+        product_id:"ms5",
+        price:313,
+        category:"Boots",
+        brand:"Diesel",
+        img:"https://cdna.lystit.com/200/250/tr/photos/ssense/478fb597/diesel-T-D-alabhama-Cb-Boos.jpeg"
+    },
+    {
+        product_id:"ms6",
+        price:396,
+        category:"Boots",
+        brand:"Hugo Boss",
+        img:"https://cdna.lystit.com/200/250/tr/photos/ssense/840ab551/hugo-BLACK-Leather-Chelsea-Boots.jpeg"
+    },
+    {
+        product_id:"ms7",
+        price:242,
+        category:"Boots",
+        brand:"Hugo Boss",
+        img:"https://cdna.lystit.com/200/250/tr/photos/hugo_boss/3858ff34/hugo-Beige-Canvas-Half-Boots-With-Suede-Trims.jpeg"
+    },
+    {
+        product_id:"ms8",
+        price:146,
+        category:"Boots",
+        brand:"Nike",
+        img:"https://cdna.lystit.com/200/250/tr/photos/lvr/191de2b4/nike-Black-Air-Max-Goaterra-20-Boots.jpeg"
+    },
+    {
+        product_id:"ms9",
+        price:218,
+        category:"Boots",
+        brand:"Nike",
+        img:"https://cdna.lystit.com/200/250/tr/photos/endclothing/1b41b23f/nike-Brown-Kelp-Air-Force-1-Boot-Sneakers.jpeg"
+    },
+    {
+        product_id:"ms10",
+        price:141,
+        category:"Boots",
+        brand:"Raplh Lauren",
+        img:"https://cdna.lystit.com/200/250/tr/photos/endclothing/c72bdcd9/polo-ralph-lauren-Tan-Talan-Suede-Chukka-Boot.jpeg"
+    },
+    {
+        product_id:"ms11",
+        price:350,
+        category:"Lace-Ups",
+        brand:"Hugo Boss",
+        img:"https://cdna.lystit.com/200/250/tr/photos/hugo_boss/04f3a2ee/boss-by-hugo-boss-Black-Oxford-Shoes-In-Patent-Leather-With-Grosgrain-Piping.jpeg"
+    },
+    {
+        product_id:"ms12",
+        price:289,
+        category:"Lace-Ups",
+        brand:"Hugo Boss",
+        img:"https://cdna.lystit.com/200/250/tr/photos/hugo_boss/0716954a/boss-by-hugo-boss-Black-Woven-leather-Derby-Shoes-With-Cushioning-Insole.jpeg"
+    },
+    {
+        product_id:"ms13",
+        price:297,
+        category:"Lace-Ups",
+        brand:"Hugo Boss",
+        img:"https://cdna.lystit.com/200/250/tr/photos/ssense/7ca049a4/hugo-BLACK-Lace-up-Derbys.jpeg"
+    },
+    {
+        product_id:"ms14",
+        price:343,
+        category:"Lace-Ups",
+        brand:"Diesel",
+        img:"https://cdna.lystit.com/200/250/tr/photos/vitkac/9b304167/diesel-BLACK-Patent-leather-Shoes.jpeg"
+    },
+    {
+        product_id:"ms15",
+        price:224,
+        category:"Lace-Ups",
+        brand:"Diesel",
+        img:"https://cdna.lystit.com/200/250/tr/photos/yoox/ff304095/diesel-Dark-blue-Lace-up-Shoes.jpeg"
+    },
+    {
+        product_id:"ms16",
+        price:344,
+        category:"Lace-Ups",
+        brand:"Diesel",
+        img:"https://cdna.lystit.com/200/250/tr/photos/farfetch/1087d50e/diesel-Black-D-konba-Lace-up-Shoes.jpeg"
+    },
+    {
+        product_id:"ms17",
+        price:471,
+        category:"Lace-Ups",
+        brand:"Hugo Boss",
+        img:"https://cdna.lystit.com/200/250/tr/photos/hugo_boss/09345561/boss-by-hugo-boss-Dark-Blue-Italian-made-Oxford-Shoes-In-Suede-With-Cap-Toe.jpeg"
+    },
+    {
+        product_id:"ms18",
+        price:471,
+        category:"Lace-Ups",
+        brand:"Hugo Boss",
+        img:"https://cdna.lystit.com/200/250/tr/photos/hugo_boss/a1965241/boss-by-hugo-boss-Dark-Brown-Italian-made-Oxford-Shoes-In-Suede-With-Cap-Toe.jpeg"
+    },
+    {
+        product_id:"ms19",
+        price:241,
+        category:"Lace-Ups",
+        brand:"Hugo Boss",
+        img:"https://cdna.lystit.com/200/250/tr/photos/hugo_boss/0612bfdb/hugo-Black-Brogue-Derby-Shoes-With-Leather-Uppers-And-Chunky-Sole.jpeg"
+    },
+    {
+        product_id:"ms20",
+        price:288,
+        category:"Lace-Ups",
+        brand:"Hugo Boss",
+        img:"https://cdna.lystit.com/200/250/tr/photos/bloomingdales/2fa64770/boss-by-hugo-boss-Black-Patent-Eastside-Plain-Toe-Oxfords.jpeg"
+    },
+    {
+        product_id:"ms21",
+        price:294,
+        category:"Lace-Ups",
+        brand:"Hugo Boss",
+        img:"https://cdna.lystit.com/200/250/tr/photos/yoox/c9fd6e51/boss-by-hugo-boss-Dark-blue-Lace-up-Shoes.jpeg"
+    },
+    {
+        product_id:"ms22",
+        price:214,
+        category:"Lace-Ups",
+        brand:"Hugo Boss",
+        img:"https://cdna.lystit.com/200/250/tr/photos/yoox/5af1d6e9/boss-by-hugo-boss-Dunkelbraun-Schnurschuh.jpeg"
+    },
+    {
+        product_id:"ms23",
+        price:34,
+        category:"Slides",
+        brand:"Nike",
+        img:"https://cdna.lystit.com/200/250/tr/photos/scottsmenswear/1ddcc9a8/nike-white-Victori-One-Slide.jpeg"
+    },
+    {
+        product_id:"ms24",
+        price:38,
+        category:"Slides",
+        brand:"Lacoste",
+        img:"https://cdna.lystit.com/200/250/tr/photos/stand-out/7bf7ee6a/lacoste-BlackWhite-Croco-119-1-Cma-Sliders.jpeg"
+    },
+    {
+        product_id:"ms25",
+        price:44,
+        category:"Slides",
+        brand:"Lacoste",
+        img:"https://cdna.lystit.com/200/250/tr/photos/stand-out/9039519d/lacoste-BlackGreen-Croco-319-4-Cma-Sliders.jpeg"
+    },
+    {
+        product_id:"ms26",
+        price:68,
+        category:"Slides",
+        brand:"Raplh Lauren",
+        img:"https://cdna.lystit.com/200/250/tr/photos/woodhouseclothing/137775f3/polo-ralph-lauren-Navy-Pony-Logo-Slides.jpeg"
+    },
+    {
+        product_id:"ms27",
+        price:34,
+        category:"Slides",
+        brand:"Nike",
+        img:"https://cdna.lystit.com/200/250/tr/photos/scottsmenswear/23c6c355/nike-white-Victori-One-Slides.jpeg"
+    },
+    {
+        product_id:"ms28",
+        price:55,
+        category:"Slides",
+        brand:"Raplh Lauren",
+        img:"https://cdna.lystit.com/200/250/tr/photos/tessuti/702a573a/polo-ralph-lauren-Green-Polo-Player-Camo-Slides.jpeg"
+    },
+    {
+        product_id:"ms29",
+        price:28,
+        category:"Slides",
+        brand:"Nike",
+        img:"https://cdna.lystit.com/200/250/tr/photos/scottsmenswear/5e6aea0e/nike-black-Victori-One-Shower-Slide.jpeg"
+    },
+    {
+        product_id:"ms30",
+        price:55,
+        category:"Slides",
+        brand:"Hugo Boss",
+        img:"https://cdna.lystit.com/200/250/tr/photos/tessuti/2a6cce30/boss-by-hugo-boss-WhiteBlack-Solar-Slides-Multi.jpeg"
+    },
+    {
+        product_id:"ms31",
+        price:49,
+        category:"Slides",
+        brand:"Lacoste",
+        img:"https://cdna.lystit.com/200/250/tr/photos/scottsmenswear/a6c250de/lacoste-black-Croco-Slides.jpeg"
+    },
+    {
+        product_id:"ms32",
+        price:79,
+        category:"Slides",
+        brand:"Hugo Boss",
+        img:"https://cdna.lystit.com/200/250/tr/photos/tessuti/01639ede/boss-by-hugo-boss-BlackWhite-Bay-Slides-Multi.jpeg"
+    },
+    {
+        product_id:"ms33",
+        price:"37",
+        category:"Slides",
+        brand:"Diesel",
+        img:"https://cdna.lystit.com/200/250/tr/photos/vitkac/5fd5e2ba/diesel-WHITE-Rubber-Slides.jpeg"
+    },
+    {
+        product_id:"ms34",
+        price:85,
+        category:"Slides",
+        brand:"Hugo Boss",
+        img:"https://cdna.lystit.com/200/250/tr/photos/allsole/cb944448/boss-by-hugo-boss--Bay-Slide-Sandals.jpeg"
+    },
+    {
+        product_id:"ms35",
+        price:195,
+        category:"Sneakers",
+        brand:"Nike",
+        img:"https://cdna.lystit.com/200/250/tr/photos/tessabit/cdf07910/nike--Air-Max-95-Kj-Sneakers.jpeg"
+    },
+    {
+        product_id:"ms36",
+        price:122,
+        category:"Sneakers",
+        brand:"Nike",
+        img:"https://cdna.lystit.com/200/250/tr/photos/scottsmenswear/17d8243a/nike-grey-React-Live-Se.jpeg"
+    },
+    {
+        product_id:"ms37",
+        price:324,
+        category:"Sneakers",
+        brand:"Nike",
+        img:"https://cdna.lystit.com/200/250/tr/photos/mclabels/52fa8dee/nike-YELLOW-Comicssun2232-Leather-Sneakers.jpeg"
+    },
+    {
+        product_id:"ms38",
+        price:168,
+        category:"Sneakers",
+        brand:"Nike",
+        img:"https://cdna.lystit.com/200/250/tr/photos/cettire/d16d8245/nike-Grey-Space-Hippie-04-Lace-up-Sneakers.jpeg"
+    },
+    {
+        product_id:"ms39",
+        price:114,
+        category:"Sneakers",
+        brand:"Raplh Lauren",
+        img:"https://cdna.lystit.com/200/250/tr/photos/mytheresa/09aa1bc4/polo-ralph-lauren-white-Court-Leather-Sneakers.jpeg"
+    },
+    {
+        product_id:"ms40",
+        price:188,
+        category:"Sneakers",
+        brand:"Diesel",
+        img:"https://cdna.lystit.com/200/250/tr/photos/ssense/d5f3291a/diesel-H-Wite-S-racer-Lc-Sneakers.jpeg"
+    },
+    {
+        product_id:"ms41",
+        price:104,
+        category:"Sneakers",
+        brand:"Raplh Lauren",
+        img:"https://cdna.lystit.com/200/250/tr/photos/allsole/aa4bd950/polo-ralph-lauren--Hanford-Vulcanised-Suede-Trainers.jpeg"
+    },
+    {
+        product_id:"ms42",
+        price:125,
+        category:"Sneakers",
+        brand:"Lacoste",
+        img:"https://cdna.lystit.com/200/250/tr/photos/stand-out/7a880db9/lacoste-WhiteBlack-L001-0722-2-Sma-Leather-Trainers.jpeg"
+    },
+    {
+        product_id:"ms43",
+        price:287,
+        category:"Sneakers",
+        brand:"Hugo Boss",
+        img:"https://cdna.lystit.com/200/250/tr/photos/woodhouseclothing/18817b14/boss-by-hugo-boss-Navy-Titanium-Runn-Sustainable-Knitted-Sock-Trainers.jpeg"
+    },
+    {
+        product_id:"ms44",
+        price:188,
+        category:"Sneakers",
+        brand:"Diesel",
+        img:"https://cdna.lystit.com/200/250/tr/photos/ssense/a324248e/diesel-T-Burgundy-Grey-S-yche-Ll-Sneakers.jpeg"
+    },
+    {
+        product_id:"ms45",
+        price:106,
+        category:"Sneakers",
+        brand:"Lacoste",
+        img:"https://cdna.lystit.com/200/250/tr/photos/stand-out/91226d5d/lacoste-NavyWhite-Bayliss-Deck-0722-1-Cma-Leather-Trainers.jpeg"
+    },
+    {
+        product_id:"ms46",
+        price:240,
+        category:"Sneakers",
+        brand:"Lacoste",
+        img:"https://cdna.lystit.com/200/250/tr/photos/ssense/19ea5958/lacoste-NVYLTBLUE-L-guard-Breaker-Sneakers.jpeg"
+    },
+    {
+        product_id:"ms47",
+        price:109,
+        category:"Sneakers",
+        brand:"Hugo Boss",
+        img:"https://cdna.lystit.com/200/250/tr/photos/hugo_boss/b8c7cc06/hugo-Black-Logo-print-Trainers-With-Branded-Red-Label.jpeg"
+    },
+    {
+        product_id:"ms48",
+        price:287,
+        category:"Sneakers",
+        brand:"Hugo Boss",
+        img:"https://cdna.lystit.com/200/250/tr/photos/woodhouseclothing/d4fc4fe3/boss-by-hugo-boss-Khaki-Bulton-Runn-Sock-Construction-Trainers.jpeg"
+    },
+    {
+        product_id:"ms49",
+        price:208,
+        category:"Sneakers",
+        brand:"Diesel",
+        img:"https://cdna.lystit.com/200/250/tr/photos/vitkac/d6ff81fc/diesel-WHITE-s-leroji-Low-Sneakers.jpeg"
+    },
+    {
+        product_id:"ms50",
+        price:92,
+        category:"Sneakers",
+        brand:"Raplh Lauren",
+        img:"https://cdna.lystit.com/200/250/tr/photos/mrporter/5d7a8291/polo-ralph-lauren-Blue-Court-Vulc-Webbing-trimmed-Leather-And-Mesh-Sneakers.jpeg"
+    }
+]
+
+let mendata = document.getElementById("box");
+let menShoesLS = JSON.parse(localStorage.getItem("cartData")) || [];
+
+
+function displayProducts(ms_prod_data) {
+    mendata.innerHTML = "";
+    ms_prod_data.forEach(function (elem) {
+        let product = document.createElement("div");
+        let product1 = document.createElement("div");
+        let product2 = document.createElement("div");
+        let product3 = document.createElement("div");
+
+        let img = document.createElement("img");
+        img.src = elem.img;
+
+        let name = document.createElement("p")
+        name.innerText = elem.brand
+
+        let product_name = document.createElement("p")
+        product_name.innerText = elem.product_name
+
+        let price = document.createElement("h3")
+        price.innerText = "$" + elem.price
+
+        let btn = document.createElement("button")
+        btn.innerText = "Add To Cart"
+        btn.addEventListener("click",function(){
+            if(btnFunc(elem.product_id)==true){
+                alert("Product added Successfully");
+                menShoesLS.push(elem);
+                // console.log(menShoesLS);
+                localStorage.setItem("cardData",JSON.stringify(menShoesLS))
+            } else {
+                alert("Product already in the cart") 
+            }
+        })
+
+        product1.append(img)
+        product2.append(name, product_name, price)
+        product3.append(btn)
+        product.append(product1, product2, product3);
+        mendata.append(product)
+
+    })
+    // localStorage.setItem("cartData",JSON.stringify(ms_prod_data))
+}
+displayProducts(ms_prod_data)
+
+
+document.querySelector("#Category").addEventListener("change", function () {
+    let val = document.querySelector("#Category").value;
+    if (val == "All") {
+        displayProducts(ms_prod_data);
+    }
+    else {
+        let filtered = ms_prod_data.filter(function (elem) {
+            return elem.category == val;
+        })
+        displayProducts(filtered);
+    }
+})
+document.querySelector("#Brand").addEventListener("change", function () {
+    let val = document.querySelector("#Brand").value;
+    if (val == "All") {
+        displayProducts(ms_prod_data);
+    }
+    else {
+        let filtered = ms_prod_data.filter(function (elem) {
+            return elem.brand == val;
+        })
+        displayProducts(filtered);
+    }
+})
+document.querySelector("#SortPrice").addEventListener("change", function () {
+    let val = document.querySelector("#SortPrice").value;
+    if (val == "Ascending") {
+        ms_prod_data.sort(function (a, b) {
+            if (a.price < b.price) return -1
+            if (a.price > b.price) return 1
+            return 0;
+        })
+        displayProducts(ms_prod_data);;
+    }
+    else if (val == "Descending") {
+        ms_prod_data.sort(function (a, b) {
+            if (a.price < b.price) return 1
+            if (a.price > b.price) return -1
+            return 0;
+        })
+        displayProducts(ms_prod_data);;
+    }
+})
+function btnFunc(id){
+    for(let i=0;i<menShoesLS.length;i++){
+        if(menShoesLS[i].product_id == id){
+            return false;
+        }
+    } 
+    return true;
+}
